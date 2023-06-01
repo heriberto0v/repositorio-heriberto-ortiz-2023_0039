93516cc8273ce4336838a1f34935427a5b2b861d
heriberto0v <133430858+heriberto0v@users.noreply.github.com>
2023-05-31 21:33:14 -0400

    .

---
+++ Diagrama de flujo del cap 2/2.13/2.13/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Función.
+El programa, al recibir como dato un valor entero, calcula el resultado de
+➥una función.
+Y: variable de tipo entero.
+X: variable de tipo real. */
+
+void main(void)
+{
+    float X;
+    int Y;
+    printf("Ingrese el valor de Y: ");
+    scanf("%d", &Y);
+    if(Y < 0 ||Y > 50)X = 0;
+    else if(Y <= 10)X =  4 / Y - Y;
+    else if (Y <= 25)X = (Y, 3) - 12;
+    else
+        X = (Y, 2) + (Y, 3) - 18;
+    printf("\n\nY = %d\tX = %8.2f", Y, X);
+}
+
---
+++ Diagrama de flujo del cap 2/2.2/2.2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Incremento de precio.
+El programa, al recibirt como dato el precio de un producto importado,
+➥incrementa 11% el mismo si este inferior a $1,500.
+PRE y NPR: variable de tipo real. */
+
+void main(void)
+{
+    float PRE, NPR;
+    printf("ingrese el precio del producto: ");
+    scanf("%f", &PRE);
+    if (PRE> 1500)
+    {
+        NPR = PRE * 1.11;
+        printf("/nNuevo precio: %7.2f",NPR);
+    }
+}
+
